92a6d3f9dd67c3bcf0f240b241bb5190e97b3bba
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-02-23 21:40:41 +0530

    starting with streamlit

---
+++ python/14-Streamlit/app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+## Title of the application
+st.title("Hello Streamlit")
+
+# Display a simple Text
+st.write("This is a simple Text")
+
