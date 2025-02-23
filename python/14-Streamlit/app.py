7cab9b11d162591ab537b0691aee27de1b5a3153
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-02-23 21:56:11 +0530

    displaying the data and line chart with streamlit

--- python/14-Streamlit/app.py
+++ python/14-Streamlit/app.py
@@ -8,3 +8,19 @@ st.title("Hello Streamlit")
 # Display a simple Text
 st.write("This is a simple Text")
 
+# Create a simple DataFrame
+
+df = pd.DataFrame({
+    'First Column': [1, 2, 3, 4],
+    'Second Column': [10, 20, 30, 40]
+})
+
+st.write("Here is the dataframe")
+st.write(df)
+
+# Create a line chart
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3), 
+    columns = ['a', 'b', 'c']
+)
+st.line_chart(chart_data)
