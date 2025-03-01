16dc082513667eded788b2fd6aa2819875abb1b6
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-03-01 09:12:17 +0530

    multiprocessing with the help of Process Pool Executor

---
+++ python/14-Multithreading and Multiprocessing/14.4-advance_multi_processing.py
@@ -0,0 +1,20 @@
+## Multiprocessing with Process Pool Executor
+
+from concurrent.futures import ProcessPoolExecutor
+import time
+
+def square_numbers(number):
+    time.sleep(1)
+    return f"Square: {number*number}"
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+if __name__ == "__main__":
+    with ProcessPoolExecutor(max_workers=3) as executor:
+        results = executor.map(square_numbers, numbers)
+
+        # for result in results:
+        #     print(result)         # This will print the result in the order they are completed 
+
+    for result in results:
+        print(result)       
