2a5699aa6813a8cda8f13b74fc386f5637b596c5
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-03-01 12:23:09 +0530

    starting with flask framework

---
+++ python/15-Flask/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+'''
+ It creates an instance of the Flask class, 
+ which will be your WSGI (Web Server Gateway Interface) application.
+'''
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return "Welcome to this Flask course"
+
+@app.route("/hello")
+def hello():
+    return "Hello User"
+
+@app.route("/index")
+def index():
+    return "Welcome to index page"
+
+if __name__=="__main__":
+    app.run(debug=True)
