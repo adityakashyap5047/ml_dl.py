0eff9088d6d0f5f7a4bb297cba5c0db2dad42f65
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-03-16 08:37:01 +0530

    loading the pickle file

--- ml-dl/5-Ridge Lasso ElasticNet Regression/End To End Project/application.py
+++ ml-dl/5-Ridge Lasso ElasticNet Regression/End To End Project/application.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, request, jsonify, render_template
+import pickle
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
 
 application = Flask(__name__)
 app = application
+
+### Loading the StandardScaler and Ridge Regression Pickle file
+ridge = pickle.load(open('models/ridge.pkl', 'rb'))
+scaler = pickle.load(open('models/scaler.pkl', 'rb'))
 
 @app.route("/")
 def hello_world():
