dc099bc3298c442bc82ac1184529b1ef2bd96406
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-04-08 01:13:41 +0530

    Taking the user input

--- Deep Learning/1. ANN/1.1-ANN Classification/app.py
+++ Deep Learning/1. ANN/1.1-ANN Classification/app.py
@@ -18,3 +18,18 @@ with open('ohe.pkl', 'rb') as file:
 with open('scaler.pkl', 'rb') as file:
     scaler = pickle.load(file)
 
+### Streamlit app
+st.title("Customer Churn Prediction")
+
+### User input
+geography = st.selectbox("Geography", ohe.categories_[0])
+gender = st.selectbox('Gender', ohe.classes_)
+age = st.slider('Age', 18, 92)
+balance = st.number_input('Balance', 0.0, 250000.0, step=100.0)
+credit_score = st.number_input('Credit Score', 0, 850, step=1)
+estimated_salary = st.number_input('Estimated Salary', 0.0, 200000.0, step=100.0)
+tenure = st.slider('Tenure', 0, 10)
+num_of_products = st.slider('Number of Products', 1, 4)
+has_cr_card = st.selectbox('Has Credit Card', [0, 1])
+is_active_member = st.selectbox('Is Active Member', [0, 1])
+is_early_exit = st.selectbox('Is Early Exit', [0, 1])
