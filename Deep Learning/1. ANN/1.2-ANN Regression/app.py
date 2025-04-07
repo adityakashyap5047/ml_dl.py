ec5b2f9789e3b4c1b195a7cfe8d3b5eb712981ca
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-04-08 03:58:28 +0530

    creating the user interface for ANN regression

---
+++ Deep Learning/1. ANN/1.2-ANN Regression/app.py
@@ -0,0 +1,67 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import pickle
+from sklearn.preprocessing import StandardScaler, OneHotEncoder, LabelEncoder
+
+
+# Load the trained model
+model = tf.keras.models.load_model('regression.h5')
+
+### Load the encoders and scaler
+with open('label_encoder_gender.pkl', 'rb') as file:
+    label_encoder_gender = pickle.load(file)
+
+with open('ohe.pkl', 'rb') as file:
+    ohe = pickle.load(file)
+
+with open('scaler.pkl', 'rb') as file:
+    scaler = pickle.load(file)
+
+
+### Streamlit app
+st.title("Customer Churn Prediction")
+
+### User input
+geography = st.selectbox("Geography", ohe.categories_[0])
+gender = st.selectbox('Gender', label_encoder_gender.classes_)
+age = st.slider('Age', 18, 92)
+balance = st.number_input('Balance', 0.0, 250000.0, step=100.0)
+credit_score = st.number_input('Credit Score', 0, 850, step=1)
+exited = st.selectbox('Is Early Exit', [0, 1])
+tenure = st.slider('Tenure', 0, 10)
+num_of_products = st.slider('Number of Products', 1, 4)
+has_cr_card = st.selectbox('Has Credit Card', [0, 1])
+is_active_member = st.selectbox('Is Active Member', [0, 1])
+
+
+### Prepare the input data
+input_data = pd.DataFrame({
+    'CreditScore': [credit_score],
+    'Gender': [label_encoder_gender.transform([gender])[0]],
+    'Age': [age],
+    'Tenure': [tenure],
+    'Balance': [balance],
+    'NumOfProducts': [num_of_products],
+    'HasCrCard': [has_cr_card],
+    'IsActiveMember': [is_active_member],
+    'Exited': [exited],
+})
+
+
+### One-hot encode the geography column
+geography_encoded = ohe.transform([[geography]]).toarray()
+geo_encoded_df = pd.DataFrame(geography_encoded, columns=ohe.get_feature_names_out(['Geography']))
+
+### Concatenate the encoded geography with the input data
+input_data = pd.concat([input_data.reset_index(drop=True), geo_encoded_df], axis=1)
+
+### Scale the input data
+input_data_scaled = scaler.transform(input_data)
+
+### Predict the estimated salary
+prediction = model.predict(input_data_scaled)
+predicted_salary = prediction[0][0]
+
+st.write(f'Predicted Estimated Salary: ${predicted_salary:.2f}')
